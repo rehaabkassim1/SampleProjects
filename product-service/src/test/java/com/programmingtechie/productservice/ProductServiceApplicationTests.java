48293b4c5b661044711e2688b615a07a8789bc55
rehaabkassim1 <33922750+rehaabkassim1@users.noreply.github.com>
2023-11-17 02:42:31 -0800

    Add files via upload

---
+++ product-service/src/test/java/com/programmingtechie/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,28 @@
+package com.programmingtechie.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+@SpringBootTest
+@Testcontainers
+class ProductServiceApplicationTests {
+
+	@Container
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:4.0.10"));
+
+	@DynamicPropertySource
+	static void setProperty(DynamicPropertyRegistry dynamicPropertyRegistry) {
+		dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+	}
+	
+	@Test
+	void shouldCreateProduct() {
+	}
+
+}
