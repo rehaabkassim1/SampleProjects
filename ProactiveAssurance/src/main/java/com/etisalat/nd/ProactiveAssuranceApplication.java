190ce6a9c6be20d1581e81bbba531ee489c67049
rehaabkassim1 <rehaabkassim1@gmail.com>
2023-12-13 22:48:23 -0800

    commit

---
+++ ProactiveAssurance/src/main/java/com/etisalat/nd/ProactiveAssuranceApplication.java
@@ -0,0 +1,13 @@
+package com.etisalat.nd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProactiveAssuranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProactiveAssuranceApplication.class, args);
+	}
+
+}
---
+++ ProactiveAssurance/src/test/java/com/etisalat/nd/ProactiveAssuranceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etisalat.nd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProactiveAssuranceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
